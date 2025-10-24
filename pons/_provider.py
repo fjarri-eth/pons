bc416e3144a67a96412bf6aefbd28e49c7a5765b
Bogdan Opanchuk <bogdan@opanchuk.net>
2025-10-24 10:57:40 -0700

    Move HTTPProvider into its own feature

--- pons/_provider.py
+++ pons/_provider.py
@@ -2,13 +2,8 @@ from abc import ABC, abstractmethod
 from collections.abc import AsyncIterator, Iterable, Mapping, Sequence
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from http import HTTPStatus
-from json import JSONDecodeError
-from typing import cast
 
-import httpx
-from compages import StructuringError
-from ethereum_rpc import RPCError, structure
+from ethereum_rpc import RPCError
 
 RPC_JSON = None | bool | int | float | str | Sequence["RPC_JSON"] | Mapping[str, "RPC_JSON"]
 """RPC requests and responses serializable to JSON."""
@@ -29,33 +24,6 @@ class ProtocolError(ABC, Exception):
 
     See the provider-specifc derived class for this exception for more details.
     """
-
-
-class HTTPError(ProtocolError):
-    """
-    Raised when the provider returns a response with a status code other than 200,
-    and no ``"error"`` field in the associated JSON data.
-    """
-
-    status: HTTPStatus
-    """The HTTP status of the response."""
-
-    message: str
-    """The response body."""
-
-    def __init__(self, status_code: int, message: str):
-        try:
-            status = HTTPStatus(status_code)
-        except ValueError:  # pragma: no cover
-            # How to handle it better? Ideally, `httpx` should have returned a parsed status
-            # in the first place, but, alas, it just gives us an integer.
-            status = HTTPStatus.INTERNAL_SERVER_ERROR
-
-        self.status = status
-        self.message = message
-
-    def __str__(self) -> str:
-        return f"HTTP status {self.status}: {self.message}"
 
 
 @dataclass
@@ -149,68 +117,3 @@ class ProviderSession(ABC):
         if not path.is_empty():
             raise ValueError(f"Expected an empty provider path, got: `{path}`")
         return await self.rpc(method, *args)
-
-
-class HTTPProvider(Provider):
-    """A provider for RPC via HTTP(S)."""
-
-    def __init__(self, url: str):
-        self._url = url
-
-    @asynccontextmanager
-    async def session(self) -> AsyncIterator["HTTPSession"]:
-        async with httpx.AsyncClient() as client:
-            yield HTTPSession(self._url, client)
-
-
-class HTTPSession(ProviderSession):
-    def __init__(self, url: str, http_client: httpx.AsyncClient):
-        self._url = url
-        self._client = http_client
-
-    def _prepare_request(self, method: str, *args: RPC_JSON) -> RPC_JSON:
-        return {"jsonrpc": "2.0", "method": method, "params": args, "id": 0}
-
-    async def rpc(self, method: str, *args: RPC_JSON) -> RPC_JSON:
-        json = self._prepare_request(method, *args)
-        try:
-            response = await self._client.post(self._url, json=json)
-        except httpx.ConnectError as exc:
-            raise ProviderError(Unreachable(str(exc))) from exc
-
-        status = response.status_code
-
-        try:
-            response_json = response.json()
-        except JSONDecodeError as exc:
-            content = response.content.decode()
-            raise ProviderError(
-                InvalidResponse(f"Expected a JSON response, got HTTP status {status}: {content}")
-            ) from exc
-
-        if not isinstance(response_json, Mapping):
-            raise ProviderError(
-                InvalidResponse(f"RPC response must be a dictionary, got: {response_json}")
-            )
-        response_json = cast("Mapping[str, RPC_JSON]", response_json)
-
-        # Note that the Eth-side errors (e.g. transaction having been reverted)
-        # will have the HTTP status 200, so we are checking for the "error" field first.
-        if "error" in response_json:
-            try:
-                error = structure(RPCError, response_json["error"])
-            except StructuringError as exc:
-                raise ProviderError(
-                    InvalidResponse(f"Failed to parse an error response: {response_json}")
-                ) from exc
-
-            raise ProviderError(error)
-
-        if status == HTTPStatus.OK:
-            if "result" in response_json:
-                return response_json["result"]
-            raise ProviderError(
-                InvalidResponse(f"`result` is not present in the response: {response_json}")
-            )
-
-        raise ProviderError(HTTPError(status, response.content.decode()))
---
+++ tests/test_http_provider.py
@@ -0,0 +1,229 @@
+import re
+from collections.abc import AsyncIterator
+from http import HTTPStatus
+from typing import Any
+
+import pytest
+import trio
+from ethereum_rpc import Amount, RPCError, RPCErrorCode
+from pytest import MonkeyPatch
+
+from pons import (
+    AccountSigner,
+    BadResponseFormat,
+    Client,
+    ClientSession,
+    ProviderError,
+    http_provider_server,  # For monkeypatching purposes
+)
+from pons._provider import RPC_JSON
+from pons.http_provider import HTTPProvider
+from pons.http_provider_server import HTTPProviderServer
+from pons.local_provider import LocalProvider
+
+
+@pytest.fixture
+async def server(
+    nursery: trio.Nursery, local_provider: LocalProvider
+) -> AsyncIterator[HTTPProviderServer]:
+    handle = HTTPProviderServer(local_provider)
+    await nursery.start(handle)
+    yield handle
+    await handle.shutdown()
+
+
+@pytest.fixture
+async def session(server: HTTPProviderServer) -> AsyncIterator[ClientSession]:
+    client = Client(server.http_provider)
+    async with client.session() as session:
+        yield session
+
+
+async def test_single_value_request(session: ClientSession) -> None:
+    assert await session.net_version() == "1"
+
+
+async def test_rpc_error(
+    session: ClientSession, local_provider: LocalProvider, monkeypatch: MonkeyPatch
+) -> None:
+    # "Happy path" for an RPC error - the reponse is well-formed, and the error code is recognized.
+
+    orig_rpc = local_provider.rpc
+
+    def mock_rpc(method: str, *args: Any) -> RPC_JSON:
+        result = orig_rpc(method, *args)
+        if method == "net_version":
+            raise RPCError.with_code(RPCErrorCode.INVALID_REQUEST, "Mock error")
+        return result
+
+    monkeypatch.setattr(local_provider, "rpc", mock_rpc)
+
+    with pytest.raises(
+        ProviderError,
+        match=re.escape("Provider error: RPC error (RPCErrorCode.INVALID_REQUEST): Mock error"),
+    ):
+        await session.rpc.net_version()
+
+
+async def test_unexpected_response_type(
+    local_provider: LocalProvider,
+    session: ClientSession,
+    monkeypatch: MonkeyPatch,
+    root_signer: AccountSigner,
+    another_signer: AccountSigner,
+) -> None:
+    tx_hash = await session.broadcast_transfer(
+        root_signer, another_signer.address, Amount.ether(10)
+    )
+
+    monkeypatch.setattr(local_provider, "rpc", lambda _method, *_args: "something")
+
+    with pytest.raises(BadResponseFormat, match="Cannot structure into"):
+        await session.rpc.eth_get_transaction_receipt(tx_hash)
+
+
+async def test_missing_field(
+    local_provider: LocalProvider,
+    session: ClientSession,
+    monkeypatch: MonkeyPatch,
+    root_signer: AccountSigner,
+    another_signer: AccountSigner,
+) -> None:
+    orig_rpc = local_provider.rpc
+
+    def mock_rpc(method: str, *args: Any) -> RPC_JSON:
+        result = orig_rpc(method, *args)
+        if method == "eth_getTransactionReceipt":
+            assert isinstance(result, dict)
+            del result["status"]
+        return result
+
+    tx_hash = await session.broadcast_transfer(
+        root_signer, another_signer.address, Amount.ether(10)
+    )
+
+    monkeypatch.setattr(local_provider, "rpc", mock_rpc)
+
+    with pytest.raises(BadResponseFormat, match="status: Missing field"):
+        await session.rpc.eth_get_transaction_receipt(tx_hash)
+
+
+async def test_none_instead_of_dict(
+    local_provider: LocalProvider,
+    session: ClientSession,
+    monkeypatch: MonkeyPatch,
+    root_signer: AccountSigner,
+    another_signer: AccountSigner,
+) -> None:
+    tx_hash = await session.broadcast_transfer(
+        root_signer, another_signer.address, Amount.ether(10)
+    )
+
+    # Check that a None can be returned in a call that expects a `dict`
+    # (the interpretation of such an event is up to the client).
+    # `eth_getTransactionReceipt` can return a None normally (if there's no receipt yet),
+    # but we force it here, just in case.
+    monkeypatch.setattr(local_provider, "rpc", lambda _method, *_args: None)
+
+    assert await session.rpc.eth_get_transaction_receipt(tx_hash) is None
+
+
+async def test_non_json_response(
+    local_provider: LocalProvider, session: ClientSession, monkeypatch: MonkeyPatch
+) -> None:
+    def faulty_net_version(_method: str, *_args: Any) -> RPC_JSON:
+        # A generic exception will generate a 500 status code
+        raise Exception("Something unexpected happened")  # noqa: TRY002
+
+    monkeypatch.setattr(local_provider, "rpc", faulty_net_version)
+
+    message = (
+        "Provider error: Expected a JSON response, got HTTP status 500: "
+        "Something unexpected happened"
+    )
+    with pytest.raises(ProviderError, match=message):
+        await session.net_version()
+
+
+async def test_no_result_field(session: ClientSession, monkeypatch: MonkeyPatch) -> None:
+    # Tests the handling of a badly formed success response without the "result" field.
+
+    orig_process_request = http_provider_server.process_request
+
+    async def faulty_process_request(*args: Any, **kwargs: Any) -> tuple[HTTPStatus, RPC_JSON]:
+        status, response = await orig_process_request(*args, **kwargs)
+        assert isinstance(response, dict)
+        del response["result"]
+        return (status, response)
+
+    monkeypatch.setattr(http_provider_server, "process_request", faulty_process_request)
+
+    with pytest.raises(
+        ProviderError, match="Provider error: `result` is not present in the response"
+    ):
+        await session.net_version()
+
+
+async def test_no_error_field(session: ClientSession, monkeypatch: MonkeyPatch) -> None:
+    # Tests the handling of a badly formed error response without the "error" field.
+
+    orig_process_request = http_provider_server.process_request
+
+    async def faulty_process_request(*args: Any, **kwargs: Any) -> tuple[HTTPStatus, RPC_JSON]:
+        _status, response = await orig_process_request(*args, **kwargs)
+        assert isinstance(response, dict)
+        del response["result"]
+        return (HTTPStatus.BAD_REQUEST, response)
+
+    monkeypatch.setattr(http_provider_server, "process_request", faulty_process_request)
+
+    with pytest.raises(
+        ProviderError, match=r"Provider error: HTTP status 400: {\"jsonrpc\":\"2.0\",\"id\":0}"
+    ):
+        await session.net_version()
+
+
+async def test_malformed_error_field(session: ClientSession, monkeypatch: MonkeyPatch) -> None:
+    # Tests the handling of a badly formed error response
+    # where the "error" field cannot be parsed as an RPCError.
+
+    orig_process_request = http_provider_server.process_request
+
+    async def faulty_process_request(*args: Any, **kwargs: Any) -> RPC_JSON:
+        _status, response = await orig_process_request(*args, **kwargs)
+        assert isinstance(response, dict)
+        del response["result"]
+        response["error"] = {"something_weird": 1}
+        return (HTTPStatus.BAD_REQUEST, response)
+
+    monkeypatch.setattr(http_provider_server, "process_request", faulty_process_request)
+
+    with pytest.raises(ProviderError, match=r"Provider error: Failed to parse an error response"):
+        await session.net_version()
+
+
+async def test_result_is_not_a_dict(session: ClientSession, monkeypatch: MonkeyPatch) -> None:
+    # Tests the handling of a badly formed provider response that is not a dictionary.
+    # Unfortunately we can't achieve that by just patching the provider, have to patch the server
+
+    async def faulty_process_request(*_args: Any, **_kwargs: Any) -> tuple[HTTPStatus, RPC_JSON]:
+        return (HTTPStatus.OK, 1)
+
+    monkeypatch.setattr(http_provider_server, "process_request", faulty_process_request)
+
+    with pytest.raises(
+        ProviderError, match="Provider error: RPC response must be a dictionary, got: 1"
+    ):
+        await session.net_version()
+
+
+async def test_unreachable_provider() -> None:
+    bad_provider = HTTPProvider("https://127.0.0.1:8889")
+    client = Client(bad_provider)
+    async with client.session() as session:
+        with trio.fail_after(1):  # Shouldn't be necessary, but just so that the test doesn't hang
+            with pytest.raises(
+                ProviderError,
+                match=r"Provider error: all attempts to connect to 127\.0\.0\.1:8889 failed",
+            ):
+                await session.net_version()
